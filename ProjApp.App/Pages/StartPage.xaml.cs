361715db18cac669dd6454c610ad447dc293e55f
Prox747 <carloproserpio1@gmail.com>
2023-02-09 22:21:20 +0100

    tentativo fallito di animare la lobby info, pero c'ero quasi
    guardare startpage

--- ProjApp.App/Pages/StartPage.xaml.cs
+++ ProjApp.App/Pages/StartPage.xaml.cs
@@ -1,4 +1,7 @@
+using Android.App;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Maui.Animations;
+using Microsoft.Maui.Controls;
 using ProjApp.ViewModel;
 #if ANDROID
 using static ProjApp.MainActivity;
@@ -20,6 +23,24 @@ public partial class StartPage : ContentPage
         base.OnAppearing();
         WeakReferenceMessenger.Default.Send(new FullScreenMessage("HideOsNavigationBar"));
     }
+
+    //PIU O MENO FUNZIONA, MI MANCA UN PEZZO    
+
+    //private async void OnClickedCreaPartitaOrJoin(object sender, EventArgs e)
+    //{
+    //    Row0.Height = 50;
+    //    Row1.Height = 50;
+    //    Row2.Height = 50;
+    //    Row3.Height = 50;
+    //    Row4.Height = 50;
+    //    await LobbyBorder.TranslateTo(TranslationX, -230, 500, Easing.CubicInOut);
+    //    LobbyBorderRow.Height = GridLength.Star;
+    //    //Row0.Height = GridLength.Auto;
+    //    //Row1.Height = GridLength.Auto;
+    //    //Row2.Height = GridLength.Auto;
+    //    //Row3.Height = GridLength.Auto;
+    //    //Row4.Height = GridLength.Auto;
+    //}
 #endif
 
 }
