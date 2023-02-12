aa1425c6e3bb9766e6fabe5e2722063d78a42fb0
Prox747 <carloproserpio1@gmail.com>
2023-02-13 00:23:33 +0100

    BOTTOM DRAWER ALMOST WORKING luca lets go

--- ProjApp.App/Pages/MainPage.xaml.cs
+++ ProjApp.App/Pages/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using ProjApp.Gioco;
 using ProjApp.MapEl;
 using ProjApp.ViewModel;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 #if ANDROID
 using static ProjApp.MainActivity;
 #endif
@@ -30,6 +31,81 @@ public partial class MainPage : ContentPage
         #if ANDROID
         WeakReferenceMessenger.Default.Send(new FullScreenMessage("HideOsNavigationBar"));
         #endif
+    }
+
+    uint duration = 300;
+    double openY = 60;
+
+    async void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (Backdrop.Opacity == 0)
+        {
+            BottomToolbar.IsVisible = true;
+            await OpenDrawer();
+        }
+        else
+        {
+            await CloseDrawer();
+            BottomToolbar.IsVisible = false;
+        }
+    }
+
+    async void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+    {
+        if (isBackdropTapEnabled)
+        {
+            await CloseDrawer();
+        }
+    }
+
+    double lastPanY = 0;
+    bool isBackdropTapEnabled = true;
+    async void PanGestureRecognizer_PanUpdated(System.Object sender, PanUpdatedEventArgs e)
+    {
+        if (e.StatusType == GestureStatus.Running)
+        {
+            isBackdropTapEnabled = false;
+            lastPanY = e.TotalY;
+            Debug.WriteLine($"Running: {e.TotalY}");
+            if (e.TotalY > 0)
+            {
+                BottomToolbar.TranslationY = openY + e.TotalY;
+            }
+
+        }
+        else if (e.StatusType == GestureStatus.Completed)
+        {
+            //Debug.WriteLine($"Completed: {e.TotalY}");
+            if (lastPanY < 70)
+            {
+                await OpenDrawer();
+            }
+            else
+            {
+                await CloseDrawer();
+            }
+            isBackdropTapEnabled = true;
+        }
+    }
+
+    async Task OpenDrawer()
+    {
+        await Task.WhenAll
+        (
+            Backdrop.FadeTo(1, length: duration),
+            BottomToolbar.TranslateTo(0, openY, length: duration, easing: Easing.CubicInOut)
+        );
+        Backdrop.IsVisible= true;
+    }
+
+    async Task CloseDrawer()
+    {
+        await Task.WhenAll
+        (
+            Backdrop.FadeTo(0, length: duration),
+            BottomToolbar.TranslateTo(0, 260, length: duration, easing: Easing.CubicInOut)
+        );
+        Backdrop.IsVisible = false;
     }
 }
 
