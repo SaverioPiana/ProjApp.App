2db786569f800979278b1fa8e0717c416f83d48c
Luca Foresti <lucapalocco@gmail.com>
2023-02-18 18:57:29 +0100

    pusho questo e carlo e e save lo prendono tutto

--- ProjApp.App/ViewModel/StartPageViewModel.cs
+++ ProjApp.App/ViewModel/StartPageViewModel.cs
@@ -49,7 +49,7 @@ namespace ProjApp.ViewModel
         [ObservableProperty]
         string codice;
         
-        public ObservableCollection<User> GiocatoriLobby = (ObservableCollection<User>)MyUser.currPartita.Players;
+        public ObservableCollection<User> GiocatoriLobby { get; set; } = (ObservableCollection<User>)MyUser.currPartita.Players;
 
         [ObservableProperty]
         bool hasCopied = false;
